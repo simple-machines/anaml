9bbebd0592d388597d288d58b2d50acf53393c44
forficate <260708+forficate@users.noreply.github.com>
2024-03-07 15:25:01 +1100

    GCP terraform minimal example (#4)

---
+++ examples/terraform/gcp-deployment/README.md
@@ -0,0 +1,3 @@
+# GCP example Terraform deployment
+
+Please see the [Anaml Quick Start GCP Terraform deployment](https://www.anaml.io/docs/anaml-gcp-terraform-quick-start/) documentation page for information on using this Terraform project.
---
+++ examples/terraform/gcp-deployment/variables.tf
@@ -0,0 +1,30 @@
+variable "hostname" {
+  type        = string
+  description = "The hostname used to access Anaml"
+}
+
+variable "initial_admin_email" {
+  type        = string
+  description = "The initial default admin email"
+}
+
+variable "initial_admin_password" {
+  type        = string
+  description = "The initial default admin password"
+}
+
+variable "zones" {
+  type        = list(string)
+  description = "The zones to host the GKE cluster in"
+}
+
+variable "region" {
+  type        = string
+  description = "The region to host the cluster in"
+}
+
+
+variable "project_id" {
+  type        = string
+  description = "The project ID to host the cluster in"
+}
